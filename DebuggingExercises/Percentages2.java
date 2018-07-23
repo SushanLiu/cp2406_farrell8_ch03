766d68c9b6e6db78b9bd56be7a5cf6646758422b
SushanLiu <30890050+SushanLiu@users.noreply.github.com>
2018-07-23 14:19:29 +0800

    pra4

---
+++ DebuggingExercises/Percentages2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Percentages2
+{
+        public static void main (String args[])
+        {
+            double num1, num2;
+            Scanner kb = new Scanner(System.in);
+
+            System.out.print("Enter an double>>");
+            num1 = kb.nextDouble();
+            System.out.print("Enter an double>>");
+            num2 = kb.nextDouble();
+            computerPercent(num1, num2);
+            computerPercent(num2, num1);
+        }
+        public static void computerPercent(double x, double y)
+        {
+            System.out.println(x + " is " + (x/y) * 100 + " percent of " + y);
+        }
+}
